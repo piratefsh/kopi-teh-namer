6ca23142f9e6c85c896b50f9e0d16b178afb0669
Sher Minn Chong <sherminn.chong@gmail.com>
2017-04-15 11:27:31 +0800

    Refactor

--- app/DrinkDisplay.jsx
+++ app/DrinkDisplay.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function DrinkDisplayPart(props){
+function DrinkDisplayPart(props) {
   return (<div className="part">
-    <div>{props.part.label || <div className="default"><br/></div>}</div>
-    <div>{props.part.definition || <br/>}</div>
-  </div>)
+    <div>{props.part.label || <div className="default"><br /></div>}</div>
+    <div>{props.part.definition || <br />}</div>
+  </div>);
 }
 
 function DrinkDisplay(props) {
-  console.log(props)
   return (
     <header>
       <div className="container">
         <div>{
           props.drink
             .parts
-            .map((part)=>{
-              return <DrinkDisplayPart 
-                part={part}/>
-          })
+            .map((part, i) => (<DrinkDisplayPart
+              key={i}
+              part={part}
+            />))
         }</div>
       </div>
     </header>
@@ -28,6 +27,10 @@ function DrinkDisplay(props) {
 
 DrinkDisplay.propTypes = {
   drink: PropTypes.object.isRequired,
+};
+
+DrinkDisplayPart.propTypes = {
+  part: PropTypes.object.isRequired,
 };
 
 export default DrinkDisplay;
