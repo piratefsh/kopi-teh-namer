fa5ac1523d16066a44d66e9f97a8048ec4a562d8
Sher Minn Chong <sherminn.chong@gmail.com>
2017-04-15 15:54:04 +0800

    Add option to hide defaults

--- app/DrinkDisplay.jsx
+++ app/DrinkDisplay.jsx
@@ -2,27 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function DrinkDisplayPart(props) {
+  if(props.hideDefault && !props.part.label){
+    return null;
+  }
   return (<div className="part">
     <div>{props.part.label || <div className="default"><br /></div>}</div>
     <div>{props.part.definition || <br />}</div>
   </div>);
 }
 
-function DrinkDisplay(props) {
+class DrinkDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hideDefaults: false
+    }
+
+    this.onShowDefaultChange = this.onShowDefaultChange.bind(this);
+  }
+
+  onShowDefaultChange(e){
+    this.setState({
+      hideDefaults: e.target.checked,
+    })
+  }
+
+  render(){
   return (
     <header>
       <div className="container">
         <div>{
-          props.drink
+          this.props.drink
             .parts
             .map((part, i) => (<DrinkDisplayPart
               key={i}
               part={part}
+              hideDefault={this.state.hideDefaults}
             />))
         }</div>
+
+        <span className='fieldset-sm right'>
+          <input type='checkbox' 
+            checked={this.state.hideDefaults ? "checked": ''}
+            onChange={this.onShowDefaultChange}
+            /><label>Hide defaults</label>
+        </span>
       </div>
     </header>
   );
+}
 }
 
 DrinkDisplay.propTypes = {
