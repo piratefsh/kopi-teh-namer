2ff2186d35673cc6ed38a9c58791f52f995dbd65
Sher Minn Chong <sherminn.chong@gmail.com>
2017-04-16 14:09:29 +0800

    Use perspectove

--- app/visualizer/parts/Liquid.jsx
+++ app/visualizer/parts/Liquid.jsx
@@ -6,27 +6,27 @@ class Liquid extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      style: this.makeStyle(props)
+      style: this.makeStyle(props),
     };
   }
 
-  componentWillReceiveProps(nextProps){
+  componentWillReceiveProps(nextProps) {
     this.state = {
       style: this.makeStyle(nextProps),
     };
   }
 
-  getTopWidth(height=0, maxHeight=30){
-    const n = 26 + 6 * (height/maxHeight)
-    return `${n}px`
+  getTopWidth(height = 0, maxHeight = 30) {
+    const n = 26 + 6 * (height / maxHeight);
+    return `${n}px`;
   }
-  makeStyle(props){
-    if(!props.color){
+  makeStyle(props) {
+    if (!props.color) {
       return {
         top: {},
         middle: {},
         bottom: {},
-      }
+      };
     }
 
     const color = Utils.hslCSS(props.color);
@@ -34,29 +34,36 @@ class Liquid extends React.Component {
       top: {
         position: 'relative',
         top: '3px',
-        width: this.getTopWidth(props.size),
+        width: '26px',
         height: '6px',
         borderRadius: '50%',
         backgroundColor: Utils.hslCSS(props.color, 10),
       },
       middle: {
         backgroundColor: color,
-        width: '32px',
+        width: '26px',
         height: `${props.size || 0}px`,
       },
       bottom: {
         position: 'relative',
         bottom: '6px',
         width: '26px',
-        height: '10px',
+        height: '14px',
         borderRadius: '50%',
         backgroundColor: color,
       },
-    }
+    };
+  }
+
+  getLiquidStyle() {
+    return {
+      width: '32px',
+      transform: 'translateX(-16px) perspective(120px) rotateX(-40deg) ',
+    };
   }
 
   render() {
-    return (<div className="liquid" style={{width: '32px'}}>
+    return (<div className="liquid" style={this.getLiquidStyle()}>
       <span className="top" style={this.state.style.top} />
       <span className="middle" style={this.state.style.middle} />
       <span className="bottom" style={this.state.style.bottom} />
